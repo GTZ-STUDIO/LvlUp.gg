2cb2639781af41ce64643460e1ed8abbf7d2bdf4
XinNie <umnie2@myumanitoba.ca>
2024-02-18 21:04:14 -0600

    can sign in sign up,get lsit of users

--- lvlgg_backend/account/serializer.py
+++ lvlgg_backend/account/serializer.py
@@ -1,12 +1,10 @@
 # serializers.py
 from rest_framework import serializers
+
 from .models import Client
 
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = [
-            "username",
-            "email",
-        ]
+        fields = "__all__"
