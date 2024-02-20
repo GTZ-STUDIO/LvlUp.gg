13e27fb519e23ba7f6a9634795f896b54877578c
NicBG <nicolasbrandtganding@gmail.com>
2024-02-20 15:42:14 -0600

    added CORS headers to backend

--- lvlgg_backend/lvlgg_backend/settings.py
+++ lvlgg_backend/lvlgg_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "lvlgg_backend.urls"
@@ -134,4 +136,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "account.Client"
 
 AUTHENTICATION_BACKENDS = ["account.backends.AccountBackend"]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 
