607b5ca979c9639a88954b529d3a26327facffdf
XinNie <umnie2@myumanitoba.ca>
2024-02-20 12:19:27 -0600

    add delete a user feature

--- lvlgg_backend/account/urls.py
+++ lvlgg_backend/account/urls.py
@@ -1,12 +1,11 @@
-
 # accounts/urls.py
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import ClientDetailView, ClientListView, SignInView
 
 urlpatterns = [
     path("", ClientListView.as_view()),
+    path("delete/<int:pk>/", ClientDetailView.as_view()),
     path("signup/", ClientDetailView.as_view()),
     path("signin/", SignInView().as_view()),
 ]
-
