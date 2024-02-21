199ced5e82f068ef62eaa9e9a4e51ce2ad82e6c1
XinNie <umnie2@myumanitoba.ca>
2024-02-20 21:08:23 -0600

    Test list view

--- lvlgg_backend/account/urls.py
+++ lvlgg_backend/account/urls.py
@@ -4,10 +4,10 @@ from django.urls import path, re_path
 from .views import ClientDetailView, ClientListView, SignInView
 
 urlpatterns = [
-    path("", ClientListView.as_view()),
+    path("", ClientListView.as_view(), name="user_list"),
     path("delete/<int:pk>/", ClientDetailView.as_view()),
     path("<int:pk>/", ClientDetailView.as_view()),
     path("update/<int:pk>/", ClientDetailView.as_view(), name="update"),
-    path("signup/", ClientDetailView.as_view()),
+    path("signup/", ClientDetailView.as_view(), name="sign_up"),
     path("signin/", SignInView.as_view()),
 ]
