1077eb24e75920b19e6443c75656817ee147c0df
NicBG <150628972+NicBG@users.noreply.github.com>
2024-02-24 14:13:09 -0600

    Update Navbar.js

--- lvlgg_frontend/src/Components/Navbar.js
+++ lvlgg_frontend/src/Components/Navbar.js
@@ -12,14 +12,6 @@ function Navbar() {
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
 
-const showButton = () => {
-    if(window.innerWidth <= 960) {
-        setButton(false)
-    } else {
-        setButton(true)
-    }
-};
-
 const handleDropDown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
