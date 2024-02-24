2fbf86be9cf4a9123971831f89093e245539fb2f
NicBG <nicolasbrandtganding@gmail.com>
2024-02-23 20:55:43 -0600

    styling change, still video template

--- lvlgg_frontend/src/Components/ExplorePage.js
+++ lvlgg_frontend/src/Components/ExplorePage.js
@@ -7,9 +7,7 @@ import './ExplorePage.css'
 function ExplorePage() {
   return (
     <div className='explore-container'>
-      <video src="/videos/video-5.mp4" autoPlay loop muted />
-      <h1>LVLUPP.GG</h1>
-      <p>Your Gaming Hub for Guides and Community Insights</p>
+      <video src="/videos/video-6.mp4" autoPlay loop muted />
       
     </div>
   )
