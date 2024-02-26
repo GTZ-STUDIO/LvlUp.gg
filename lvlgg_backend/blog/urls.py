213cdce70ce3c6d57dbbc2eaa169d673bca49503
XinNie <umnie2@myumanitoba.ca>
2024-02-25 20:41:31 -0600

    delete

--- lvlgg_backend/blog/urls.py
+++
@@ -1,7 +0,0 @@
-# accounts/urls.py
-from django.urls import path
-from .views import BlogDetailView
-
-urlpatterns = [
-    path("blog/", BlogDetailView.as_view()),
-]
--- lvlgg_backend/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-
-# Create your views here.
-class BlogDetailView(View):
-
-    pass
