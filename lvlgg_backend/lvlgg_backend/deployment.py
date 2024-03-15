22546237712f0b21e5e89277ffbb3c2646ad6fa6
Gabriel Coloma <gabrielzaccipc@gmail.com>
2024-03-14 20:27:46 -0500

    Fix typo in deployment.py

--- lvlgg_backend/lvlgg_backend/deployment.py
+++ lvlgg_backend/lvlgg_backend/deployment.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
-SECRET_KEY = os.environ['MY_SEECRET_KEY']
+SECRET_KEY = os.environ['MY_SECRET_KEY']
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
