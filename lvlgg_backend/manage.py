09f330e6370a4c5057b9f6a734aade5c63e88df6
Gabriel Coloma <gabrielzaccipc@gmail.com>
2024-03-19 20:29:15 -0500

    Fix manage.py

--- lvlgg_backend/manage.py
+++ lvlgg_backend/manage.py
@@ -7,7 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     settings_module = 'lvlgg_backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'lvlgg_backend.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lvlgg_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
